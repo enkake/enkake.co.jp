96b8e005bdb637567746a6e4afa56fbbb6d366f5
Atsushi Nagase <a@ngs.io>
2022-07-12 10:24:29 +0900

    Fix SEO

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { FC } from "react"
+import { Helmet } from "react-helmet"
 
 interface Props {
   title: string
@@ -8,11 +9,11 @@ interface Props {
 }
 
 export const SEO: FC<Props> = ({ title, description, image }) => {
-  return <>
+  return <Helmet>
     <title>{title}</title>
     <meta name="title" content={title} />
     {description && <meta name="description" content={description} />}
     {description && <meta property="og:description" content={description} />}
     {image && <meta property="og:image" content={image} />}
-  </>
+  </Helmet>
 }
