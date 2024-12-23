d7ca585b7019682bc8c9df78c7c3722635bb6714
Atsushi NAGASE <a@ngs.io>
2024-12-23 16:31:16 +0900

    Re-init gatsby project

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
-import path from "path";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `enkake, inc.`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `enkake Inc.`,
+    siteUrl: `https://enkake.co.jp/`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     {
@@ -18,29 +14,16 @@ const config: GatsbyConfig = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-emotion",
-    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-google-gtag",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-PLRS35PR",
       },
     },
   ],
